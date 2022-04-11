c4000536270bb04ff18dd3583df501581acd79f9
Achim Völz <achim.voelz.1991@gmail.com>
2022-04-11 17:47:23 +0200

    adde boolean attributes to AttributeLink

--- lib/links/AttributeLink.ts
+++ lib/links/AttributeLink.ts
@@ -1,6 +1,33 @@
 import State from "../states/State";
 import StateLink from "./StateLink";
 
+const booleanAttributes = ["allowfullscreen",
+    "allowpaymentrequest",
+    "async",
+    "autofocus",
+    "autoplay",
+    "checked",
+    "controls",
+    "default",
+    "defer",
+    "disabled",
+    "formnovalidate",
+    "hidden",
+    "ismap",
+    "itemscope",
+    "loop",
+    "multiple",
+    "muted",
+    "nomodule",
+    "novalidate",
+    "open",
+    "playsinline",
+    "readonly",
+    "required",
+    "reversed",
+    "selected",
+    "truespeed"]
+
 export default class AttributeLink implements StateLink {
 
     private element: Element;
@@ -8,6 +35,7 @@ export default class AttributeLink implements StateLink {
     private valueFunction: () => string;
     private states: State<any>[];
     private staticValue: string;
+    private isBooleanAttribute: boolean;
 
     constructor(element: Element, attributeName: string, valueFunction: () => string, states: State<any>[]) {
         this.element = element;
@@ -16,13 +44,17 @@ export default class AttributeLink implements StateLink {
         this.states = states;
 
         this.staticValue = element.getAttribute(this.attributeName) ?? "";
+        this.isBooleanAttribute = booleanAttributes.includes(this.attributeName);
 
         this.update();
     }
 
     update(): void {
+        this.element.removeAttribute(this.attributeName);
         const value = this.valueFunction();
-        this.element.setAttribute(this.attributeName, this.staticValue ? [this.staticValue, value].join(" ") : value);
+        if (!this.isBooleanAttribute || value === "true") {
+            this.element.setAttribute(this.attributeName, this.staticValue ? [this.staticValue, value].join(" ") : value);
+        }
     }
 
     destroy(): void {
